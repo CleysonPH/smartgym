4ff8aa7807e565b019fdfc25e80dc6c27b67e454
Cleyson <cleysonph@gmail.com>
2023-12-24 17:55:21 -0300

    add ping endpoint

---
+++ src/main/java/dev/cleysonph/smartgym/api/v1/common/dtos/MessageResponse.java
@@ -0,0 +1,16 @@
+package dev.cleysonph.smartgym.api.v1.common.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageResponse {
+
+    private String message;
+    
+}
